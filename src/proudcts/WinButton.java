19aa55e0a805a8452b806fe5b85d991768580e4d
gyennifer751-create <gyennifer751@gmail.com>
2026-02-17 20:56:29 -0500

    Create WinButton.java

---
+++ src/proudcts/WinButton.java
@@ -0,0 +1,7 @@
+public class WinButton implements Button {
+    @Override
+    public void paint() {
+        // TODO: Implementar la impresión en consola para Windows
+        System.out.println("Error: Método no implementado");
+    }
+}
